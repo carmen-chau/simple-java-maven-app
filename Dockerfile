84b02fba07bf092da6b84ad1ae8f1852a6f67056
carmen-chau <carmenjchau@gmail.com>
2025-06-07 10:22:33 -0400

    added dockerfiles

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+# Dockerfile
+FROM python:3.11-slim
+RUN pip install flask
+CMD ["python3"]
--- nexusiq/artifact-scanner/pipeline.groovy
+++ nexusiq/artifact-scanner/pipeline.groovy
@@ -2,10 +2,22 @@ pipeline {
     agent any
 
     stages {
-        stage ("Test Stage") {
+        stage('Build Docker Image') {
             steps {
                 script {
-                    echo "Hello World"
+                    echo "Building Docker image from repo root..."
+                    docker.build('my-custom-image', '../../')
+                }
+            }
+        }
+
+        stage('Run Container') {
+            steps {
+                script {
+                    docker.image('my-custom-image').inside {
+                        sh 'echo "Inside the custom Docker container!"'
+                        sh 'python3 --version || echo "Python not found"'
+                    }
                 }
             }
         }
